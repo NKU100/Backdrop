7c5b3a1c3f75bf936d2a18a8a6b90ba2930e8dcd
NKU100 <21164383+NKU100@users.noreply.github.com>
2026-04-01 23:03:51 +0800

    Migrate to Kotlin Multiplatform (androidTarget only)

--- backdrop/build.gradle.kts
+++ backdrop/build.gradle.kts
@@ -1,77 +1,31 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.multiplatform)
+    id("com.android.kotlin.multiplatform.library")
+    alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.kotlin.compose)
-    id("com.vanniktech.maven.publish")
 }
 
-android {
-    namespace = "com.kyant.backdrop"
-    compileSdk {
-        version = release(36)
-    }
-    buildToolsVersion = "36.1.0"
-
-    defaultConfig {
-        minSdk = 21
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    buildFeatures {
-        compose = true
-    }
-}
+group = "com.kyant.backdrop"
+version = "1.0.6-kmp"
 
 kotlin {
-    jvmToolchain(21)
+    androidLibrary {
+        namespace = "com.kyant.backdrop"
+        compileSdk = 36
+        minSdk = 21
+    }
+
     compilerOptions {
         freeCompilerArgs.addAll(
             "-Xcontext-parameters"
         )
     }
-}
 
-dependencies {
-    implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.kyant.shapes)
-}
-
-mavenPublishing {
-    publishToMavenCentral()
-    signAllPublications()
-
-    coordinates("io.github.kyant0", "backdrop", "1.0.6")
-
-    pom {
-        name.set("Backdrop")
-        description.set("Jetpack Compose blur and Liquid Glass effects")
-        inceptionYear.set("2025")
-        url.set("https://github.com/Kyant0/AndroidLiquidGlass")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("repo")
-            }
-        }
-        developers {
-            developer {
-                id.set("Kyant0")
-                name.set("Kyant")
-                url.set("https://github.com/Kyant0")
-            }
-        }
-        scm {
-            url.set("https://github.com/Kyant0/AndroidLiquidGlass")
-            connection.set("scm:git:git://github.com/Kyant0/AndroidLiquidGlass.git")
-            developerConnection.set("scm:git:ssh://git@github.com/Kyant0/AndroidLiquidGlass.git")
+    sourceSets {
+        androidMain.dependencies {
+            implementation(compose.foundation)
+            implementation(compose.ui)
+            implementation(libs.kyant.shapes)
         }
     }
 }
